040530cedbd4d83dcd9f2800fe6d6262513eec02
madhuch <madhuch@ojt18-PC.qedge.com>
2020-02-13 13:12:30 +0530

    Initial commit

---
+++ src/main/java/utilities/PropertyFileUtil.java
@@ -0,0 +1,16 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertyFileUtil {
+	
+	public static  String getValueForKey(String Key) throws Exception {
+		FileInputStream fis=new FileInputStream("./PropertiesFile/Environment.properties");
+		Properties ConfigProperties=new Properties();
+		ConfigProperties .load(fis);
+		return ConfigProperties.getProperty(Key);
+		
+	}
+
+}
